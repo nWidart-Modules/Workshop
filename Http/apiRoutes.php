8f85b11f1783f720dc6b3ba33bd28dc9dbf79a17
Nicolas Widart <n.widart@gmail.com>
2016-06-30 19:05:14 +0200

    Protecting api routes with api admin middleware

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,5 +3,13 @@
 use Illuminate\Routing\Router;
 /** @var Router $router */
 
-$router->post('/workshop/modules/{module}/publish', ['as' => 'api.workshop.module.publish', 'uses' => 'ModulesController@publishAssets']);
-$router->post('/workshop/themes/{theme}/publish', ['as' => 'api.workshop.theme.publish', 'uses' => 'ThemeController@publishAssets']);
+$router->group(['prefix' => 'workshop', 'middleware' => 'api.token.admin'], function (Router $router) {
+    $router->post('modules/{module}/publish', [
+        'as' => 'api.workshop.module.publish',
+        'uses' => 'ModulesController@publishAssets',
+    ]);
+    $router->post('themes/{theme}/publish', [
+        'as' => 'api.workshop.theme.publish',
+        'uses' => 'ThemeController@publishAssets',
+    ]);
+});
